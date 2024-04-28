b763c25bbc4ea4012462b41a16d49343698e33b5
sergiobando <felipesero@gmail.com>
2024-04-28 15:39:49 -0500

    'add:models_urls'

--- project/App/views.py
+++ project/App/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 from . import models
 
-def home(request): ...
+def home(request):
+    query = models.Comision.objects.all()
+    context = {'comisiones':query}
+    return render(request, 'App/index.html', context)
