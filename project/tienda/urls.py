d50bc6809fd753275ee6814943c0af9627386eb6
sergiobando <felipesero@gmail.com>
2024-04-29 20:33:05 -0500

    add: app_tienda

---
+++ project/tienda/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "producto"
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("productocategoria/create/", views.productocategoria_create, name="productocategoria_create"),
+]
