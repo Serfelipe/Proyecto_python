01b177548a3877427e19bad2c7d941b4b6d7265b
Sergio <felipesero@gmail.com>
2024-06-14 00:41:05 -0500

    Ajustes vistas navbar

--- project/core/urls.py
+++ project/core/urls.py
@@ -12,8 +12,8 @@ app_name = "core"
 urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.CustomLoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
+    path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
     path("contacto/", views.contacto, name="contacto"),
 ]
 if settings.DEBUG:
